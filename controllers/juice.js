ef312273f455507c0d6d600cf9b6ed0689af1129
Nageshaluri <Nagesh.aluri9@gmail.com>
2022-11-10 19:33:16 -0600

    showed one variable by usng url

--- controllers/juice.js
+++ controllers/juice.js
@@ -29,11 +29,18 @@ exports.juice_view_all_Page = async function (req, res) {
 
 
 // for a specific Costume.
-exports.juice_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Juice detail: ' + req.params.id);
-};
-// Handle Costume create on POST.
-// Handle Costume create on POST.
+exports.juice_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await Juice.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+// Handle Juice create on POST.
+// Handle Juice create on POST.
 exports.juice_create_post = async function(req, res) {
     console.log(req.body)
     let document = new Juice();
